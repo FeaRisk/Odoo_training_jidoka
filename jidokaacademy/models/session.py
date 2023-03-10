a362ee7298c6fcdd3714771b4902a7cd870c5b6b
Kris Adiwinata <99080684+FeaRisk@users.noreply.github.com>
2023-03-10 14:12:50 +0700

    Add state validation and chatter

--- jidokaacademy/models/session.py
+++ jidokaacademy/models/session.py
@@ -3,21 +3,45 @@ from odoo.exceptions import ValidationError
 
 class session(models.Model):
     _name = 'jidokaacademy.session'
+    _inherit = 'mail.thread'
     _description = 'jidokaacademy.session'
   
-    name = fields.Char(string='Title', required=True)
-    start_date = fields.Date(string='Start Date', default= fields.Date.today())
-    duration = fields.Float(string='Duration')
-    number_of_seats = fields.Float('Number of seats')
-    description = fields.Text('Description')
+    name = fields.Char(string='Title', track_visibility='onchange', required=True)
+    start_date = fields.Date(string='Start Date', default= fields.Date.today(), track_visibility='onchange')
+    duration = fields.Float(string='Duration', track_visibility='onchange')
+    number_of_seats = fields.Float('Number of seats', track_visibility='onchange')
+    description = fields.Text('Description', track_visibility='onchange')
     partner_id = fields.Many2one(
-        'res.partner', string='Instructor', domain="[('is_instructor','=',True)]")
+        'res.partner', string='Instructor', track_visibility='onchange', domain="[('is_instructor','=',True)]")
     partner_ids = fields.Many2many('res.partner', string='Attendees')
     course_id = fields.Many2one(
-        'jidokaacademy.course', string='Course', required=True)
+        'jidokaacademy.course', string='Course', required=True, track_visibility='onchange')
     taken_seats = fields.Float("Taken Seats", compute='_count_taken_seats')
-    active = fields.Boolean(default=True)
+    active = fields.Boolean(default=True, track_visibility='onchange')
     number_of_attendees = fields.Float('Number of Attendees', compute='count_attendees', store=True)
+    state = fields.Selection(string='Status', selection=[
+        ('draft', 'Draft'),
+        ('confirm', 'Confirm'),
+        ('done', 'Done'),
+        ('cancel', 'Cancel'),
+        ], readonly=True, default='draft', required=True, track_visibility='onchange')
+    
+    def action_confirm(self):
+        self.write({'state': 'confirm'})
+    
+    def action_done(self):
+        self.write({'state': 'done'})
+
+    def action_cancel(self):
+        self.write({'state': 'cancel'})
+    
+    def action_reset(self):
+        self.write({'state': 'draft'})
+        
+    def unlink(self):
+        if self.filtered(lambda line: line.state != 'draft'):
+            raise ValidationError("Tidak dapat hapus data selain Draft")
+        return super(session, self).unlink()
 
     @api.depends('partner_ids')
     def count_attendees(self):
